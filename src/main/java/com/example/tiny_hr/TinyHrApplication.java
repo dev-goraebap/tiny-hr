15ead975c1408b4506f53e63a6babad1ff376395
dev-goraebap <dev.goraebap@gmail.com>
2026-06-06 17:28:13 +0900

    init project

---
+++ src/main/java/com/example/tiny_hr/TinyHrApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiny_hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyHrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tiny_hr/TinyHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiny_hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
